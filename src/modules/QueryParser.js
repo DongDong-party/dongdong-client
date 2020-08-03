3c0b199f8e7c6d485068e23530c3ebe276c559fa
JeongWon Liee <khjvvv7@gmail.com>
2020-08-04 01:50:15 +0900

    feat: make QueryParser

---
+++ src/modules/QueryParser.js
@@ -0,0 +1,15 @@
+class QueryParser {
+  static getQueryStringObject() {
+    var queryString = window.location.search.substr(1).split("&");
+    if (queryString == "") return {};
+    var queryObject = {};
+    for (var i = 0; i < queryString.length; ++i) {
+      var p = queryString[i].split("=", 2);
+      if (p.length == 1) queryObject[p[0]] = "";
+      else queryObject[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return queryObject;
+  }
+}
+
+export default QueryParser;
