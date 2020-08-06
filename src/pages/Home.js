56fba28d3caa1ce60a92d865f0871be6e34055a0
minkyung <mnk918p@gmail.com>
2020-08-06 17:20:40 +0900

    style: add class for store list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react';
 
-import Banner from "../components/Banner";
-import Navigation from "../components/nav/Navigation";
-import SearchBar from "../components/SearchBar";
-import StoreList from "../components/StoreList";
-import CategoryList from "../components/CategoryList";
+import Banner from '../components/Banner';
+import Navigation from '../components/nav/Navigation';
+import SearchBar from '../components/SearchBar';
+import StoreList from '../components/StoreList';
+import CategoryList from '../components/CategoryList';
 
-import "../assets/scss/pages/home.scss";
+import '../assets/scss/pages/home.scss';
 
 class Home extends React.Component {
   // =====핸들러 함수===== //
-  handleSearch = (data) => {
-    this.props.history.push("/search", { keyword: data.keyword });
+  handleSearch = data => {
+    this.props.history.push('/search', { keyword: data.keyword });
   };
 
   // =======렌더링======= //
@@ -21,9 +21,13 @@ class Home extends React.Component {
         <div className="home-wrapper">
           <Navigation />
           <Banner />
-          <SearchBar handleSearch={this.handleSearch} />
-          <CategoryList />
-          <StoreList />
+          <div className="body-wrapper">
+            <SearchBar handleSearch={this.handleSearch} />
+            <CategoryList />
+            <div className="store-wrapper-one">
+              <StoreList />
+            </div>
+          </div>
         </div>
       </section>
     );
