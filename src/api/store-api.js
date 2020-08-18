8f0a287a941b09f8d219db9db7a150de33212f65
ddongule <mnk918p@gmail.com>
2020-08-19 02:00:31 +0900

    fix: show list in category page

--- src/api/store-api.js
+++ src/api/store-api.js
@@ -1,28 +1,35 @@
-import axios from "axios";
-import ApiHost from "../modules/api-host";
-import QueryManager from "../modules/query-manager";
+import axios from 'axios';
+import ApiHost from '../modules/api-host';
+import QueryManager from '../modules/query-manager';
 
 class StoreApi {
   static async getData(query) {
-    console.log(query, "call");
-    if (query === undefined) {
-      // 쿼리가 없으면 전체 리스트
-      return await this.getList();
-    } else {
-      // 쿼리가 있으면 검색
-      return await this.search(query);
+    // console.log(query, 'call');
+    console.log(query);
+    try {
+      if (query === undefined) {
+        console.log('전체리스트');
+        // 쿼리가 없으면 전체 리스트
+        return await this.getList();
+      } else {
+        // 쿼리가 있으면 검색
+        console.log('검색');
+        return await this.search(query);
+      }
+    } catch (e) {
+      console.log(e);
     }
   }
 
   static async getList() {
-    return await axios.get(ApiHost.getHost() + "api/v1/store");
+    return await axios.get(ApiHost.getHost() + 'api/v1/store');
   }
 
   static async search(query) {
     const queryString = QueryManager.makeQueryString(query);
 
     return await axios.get(
-      ApiHost.getHost() + "api/v1/store/search?" + queryString
+      ApiHost.getHost() + 'api/v1/store/search?' + queryString,
     );
   }
 }
