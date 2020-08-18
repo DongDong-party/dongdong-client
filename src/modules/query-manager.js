20350b3c9fe3af2e814da3da40f592425c48060a
ddongule <mnk918p@gmail.com>
2020-08-19 02:01:31 +0900

    fix: equal operators

--- src/modules/query-manager.js
+++ src/modules/query-manager.js
@@ -1,20 +1,20 @@
 class QueryManager {
   static getQueryStringObject() {
-    var queryString = window.location.search.substr(1).split("&");
-    if (queryString == "") return {};
+    var queryString = window.location.search.substr(1).split('&');
+    if (queryString === '') return {};
     var queryObject = {};
     for (var i = 0; i < queryString.length; ++i) {
-      var p = queryString[i].split("=", 2);
-      if (p.length == 1) queryObject[p[0]] = "";
-      else queryObject[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+      var p = queryString[i].split('=', 2);
+      if (p.length === 1) queryObject[p[0]] = '';
+      else queryObject[p[0]] = decodeURIComponent(p[1].replace(/\+/g, ' '));
     }
     return queryObject;
   }
 
   static makeQueryString(obj) {
     return Object.entries(obj)
-      .map((e) => e.join("="))
-      .join("&");
+      .map((e) => e.join('='))
+      .join('&');
   }
 }
 
